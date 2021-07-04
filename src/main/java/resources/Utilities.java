36326da8dc63589c0149bace00d8c8eadfd4e885
RamdeepSingh <ramdeepmansa@gmail.com>
2021-07-04 18:31:38 +0530

    headless options

--- src/main/java/resources/Utilities.java
+++ src/main/java/resources/Utilities.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -26,18 +27,24 @@ public class Utilities {
 		p = new Properties();
 		FileInputStream fis = new FileInputStream(path + "\\src\\main\\java\\resources\\enviornment.properties");
 		p.load(fis);
-		String browserName = System.getProperty("browser"); /*p.getProperty("browser")*/;
+		String browserName = System.getProperty("browser");
+		/* p.getProperty("browser") */;
 		String driverPath = p.getProperty("driverPath");
 
 		if (browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", path + driverPath + "chromedriver.exe");
 			drive = new ChromeDriver();
 		}
 
 		else if (browserName.equalsIgnoreCase("firefox")) {
-			System.out.println("reached here");
 			System.setProperty("webdriver.gecko.driver", path + driverPath + "geckodriver.exe");
 			drive = new FirefoxDriver();
+		}
+
+		else if (browserName.equalsIgnoreCase("headlesschrome")) {
+			System.setProperty("webdriver.chrome.driver", path + driverPath + "chromedriver.exe");
+			ChromeOptions headlessBrowser = new ChromeOptions();
+			headlessBrowser.addArguments("headless");
+			drive = new ChromeDriver(headlessBrowser);
 		}
 
 		else {
